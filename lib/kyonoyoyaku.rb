989f33319b412f24ed9542574ed0c5efc036ae16
INO <ink.avc@gmail.com>
2025-05-17 12:36:27 +0900

    feature/1 Gemの情報を表示する

--- lib/kyonoyoyaku.rb
+++ lib/kyonoyoyaku.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "kyonoyoyaku/version"
+require_relative "kyonoyoyaku/info"
 
 module Kyonoyoyaku
   class Error < StandardError; end
---
+++ lib/kyonoyoyaku/info.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Kyonoyoyaku
+  INFO = "このGemは今日の予約を管理します。"
+end
